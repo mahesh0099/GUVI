ab1e1d021926f1b3da7c3ceae3ceb514ba289904
mahesh0099 <41848002+mahesh0099@users.noreply.github.com>
2020-03-31 19:35:02 +0530

    Create 767.py

---
+++ python/Absolute Beginner/767.py
@@ -0,0 +1,41 @@
+''' You are given three numbers A, B & C. Print the largest amongst these three numbers.
+
+Input Description:
+Three numbers are provided to you.
+
+Output Description:
+Find and print the largest among the three
+
+Sample Input :
+1
+2
+3
+
+Sample Output :
+3 '''
+
+
+
+a=input("")
+b=input("")
+c=input("")
+
+if (a>b)&(a>c):
+  largestNum=a
+elif (b>c)&(b>a):
+  largestNum=b
+elif (c>a)&(c>b):
+  largestNum=c
+print(largestNum)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
