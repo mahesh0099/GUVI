c951642159b7733db575d19a45e4b4dab252987b
mahesh0099 <41848002+mahesh0099@users.noreply.github.com>
2020-04-01 09:18:43 +0530

    Create 114.py

---
+++ python/basics/114.py
@@ -0,0 +1,12 @@
+'''Given a number N, print the Bitwise NOT of that number.
+Input Size : 1 <= N <= 10000
+Sample Testcase :
+INPUT
+5
+OUTPUT
+-6 '''
+
+
+a=int(input(""))
+result=~a
+print(result)
